2d17e6555d8e4f7c0b0dad6326d5ff368ec3d5fb
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-08-25 16:19:22 -0400

    added writeFile

--- index.js
+++ index.js
@@ -38,51 +38,58 @@ Github: [${answers.github}](https://www.github.com/${answers.github})
 If you have further questions, please email me at [${email}](mailto:${email})
 `;
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "title",
-    message: "Please enter your projects's name",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Please enter the description",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "Please enter the installation instructions",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "Please enter usage information",
-  },
-  {
-    type: "input",
-    name: "contributing",
-    message: "Please enter what the user needs to know about contributing",
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: "Please enter what command should be ran to run test",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Please select what kind of license your project needs",
-    choices: ["MIT", "GNU AGPLv3", "GNU GPLv3", "Apache 2.0", "none"],
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "Please enter your github username",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Please enter your email",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Please enter your projects's name",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please enter the description",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please enter the installation instructions",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please enter usage information",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Please enter what the user needs to know about contributing",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Please enter what command should be ran to run test",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Please select what kind of license your project needs",
+      choices: ["MIT", "GNU AGPLv3", "GNU GPLv3", "Apache 2.0", "none"],
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter your github username",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your email",
+    },
+  ])
+  .then((answers) => {
+    const readmeContent = generateREADME(answers);
+    fs.writeFile("README1.md", readmeContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created README!")
+    );
+  });
